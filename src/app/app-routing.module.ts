4561e7658685e38713d51ce059b7eab633e7bfe3
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2022-03-16 15:25:15 -0300

    Cronfigurado componente Inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemasComponent } from './temas/temas.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'entrar', pathMatch: 'full'},
   {path: 'entrar', component: EntrarComponent},
   {path: 'cadastrar', component: CadastrarComponent},
   {path: 'inicio', component: InicioComponent},
+  {path: 'temas', component: TemasComponent},
 ];
 
 @NgModule({
