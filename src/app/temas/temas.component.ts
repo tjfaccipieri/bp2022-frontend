161eaefbaaf3f7f9392ffb3359dbb131ad492770
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2022-03-19 09:18:40 -0300

    Colocado if de navegação

--- src/app/temas/temas.component.ts
+++ src/app/temas/temas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-temas',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemasComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0,0)
+
+    if(environment.token == '') {
+      alert('Você precisa estar logado para acessar essa tela')
+      this.router.navigate(['/entrar'])
+    }
   }
 
 }
